4496daf9a4483b94f570d2527988da6a2bbe7235
Wawa <Wawa@172.16.126.22>
2018-11-05 21:44:54 +0100

    test algo fourmis

--- src/tsp/TSPSolver.java
+++ src/tsp/TSPSolver.java
@@ -74,7 +74,7 @@ public class TSPSolver extends AlgoFourmis {
 	
 	
 	public void solve() throws Exception{
-		
+		/*
 		Solution sol = new Solution(m_instance);
 		PlusProcheVoisin p = new PlusProcheVoisin(m_instance,m_timeLimit,sol);
 		
@@ -87,8 +87,87 @@ public class TSPSolver extends AlgoFourmis {
 		}
 		sol.evaluate();
 		this.m_solution = sol;
+		*/
+		Solution Sol = new Solution(m_instance);
 		
+		int[] result = full_algo_fourmis(this.m_instance,this.m_solution,this.m_timeLimit);
+		int n = result.length;
+		int j = 0;
+		int[] temp = new int[result.length];
+		
+		for(int i=0;i<n;i++) {
+			if(result[i]==0) {
+				j=i;
+			}
+		}
+		
+		for(int i=0;i<n-1;i++) {
+			if(i>=j) {
+				temp[i-j]=result[i];
+			}else {
+				temp[n-i+j-2]=result[i];
+				
+			}
+		}
+		temp[n-1]=0;
+		temp[n-2]=result[0];
+		
+		for(int i=0;i<temp.length;i++) {
+			Sol.setCityPosition(temp[i], i);
+			System.out.println(temp[i]);
+		}
+					
+	
+		
+		Sol.evaluate();
+		this.m_solution = Sol;
 	}
+		
+
+		/*
+
+		int n = m_instance.getNbCities();
+		Solution Sol = new Solution(m_instance);
+		ArrayList<Integer> SolutionListe = new ArrayList<Integer>();
+		long[][] m_distances = m_instance.getDistances();
+		ArrayList<Integer> v_visites = new ArrayList<Integer>(); //villes visites
+		ArrayList<Integer> v_avisiter = new ArrayList<Integer>(); //villes a visiter
+		int villeInitiale = 0;
+		int villeactuelle=0;
+		v_visites.add(villeactuelle);
+		int k=0;
+		Sol.setCityPosition(villeactuelle, k);
+		Sol.setCityPosition(villeactuelle, n);
+		SolutionListe.add(villeactuelle);
+		for (int i = 1; i < n; i++) {
+			v_avisiter.add(i);
+		}
+	
+	
+		while (v_avisiter.size()>0) {
+			int ville=v_avisiter.get(0); // recherche de la nouvelle ville à visiter
+			long distance=m_distances[villeactuelle][v_avisiter.get(0)];
+			for ( int u : v_avisiter) {//recherche parmis toutes les villes non visitees
+				if (distance>m_distances[villeactuelle][u]) {
+					distance = m_distances[villeactuelle][u];
+					ville=u;
+				}
+			}
+				v_visites.add(ville);
+				Integer city = ville;
+				v_avisiter.remove(city);
+				k+=1;
+				Sol.setCityPosition(ville, k);
+				SolutionListe.add(ville);
+				villeactuelle=ville;
+		}*/
+
+	
+	
+	/*
+	 * 
+	 */
+	
 		
 	
 	
