ee0ae1cef95566a4451bd657ef3cf7d58f342a1b
gmessi16 <gregoire.messiant@hotmail.fr>
2018-11-05 18:23:17 +0100

    New classe

--- src/tsp/PlusProcheVoisin.java
+++ src/tsp/PlusProcheVoisin.java
@@ -1,5 +1,7 @@
 package tsp;
 
+import java.util.ArrayList;
+
 public class PlusProcheVoisin {
 
 	
@@ -13,39 +15,76 @@ public class PlusProcheVoisin {
 		this.m_timeLimit = m_timeLimit;
 	}
 	
-	public 
-	int n = m_instance.getNbCities();
-	Solution Sol = new Solution(m_instance);
-	long[][] m_distances = m_instance.getDistances();
-	ArrayList<Integer> v_visites = new ArrayList<Integer>(); //villes visites
-	ArrayList<Integer> v_avisiter = new ArrayList<Integer>(); //villes a visiter
-	int villeactuelle=0;
-	v_visites.add(villeactuelle);
-	int k=0;
-	Sol.setCityPosition(villeactuelle, k);
-	Sol.setCityPosition(villeactuelle, n);
-	for (int i = 1; i < n; i++) {
-		v_avisiter.add(i);
+	public ArrayList<Integer> PlusProcheVoisinCalcul() throws Exception {
+		int n = m_instance.getNbCities();
+		Solution Sol = new Solution(m_instance);
+		ArrayList<Integer> SolutionListe = new ArrayList<Integer>();
+		long[][] m_distances = m_instance.getDistances();
+		ArrayList<Integer> v_visites = new ArrayList<Integer>(); //villes visites
+		ArrayList<Integer> v_avisiter = new ArrayList<Integer>(); //villes a visiter
+		int villeInitiale = 0;
+		int villeactuelle=0;
+		v_visites.add(villeactuelle);
+		int k=0;
+		Sol.setCityPosition(villeactuelle, k);
+		Sol.setCityPosition(villeactuelle, n);
+		SolutionListe.add(villeactuelle);
+		for (int i = 1; i < n; i++) {
+			v_avisiter.add(i);
+		}
+	
+	
+		while (v_avisiter.size()>0) {
+			int ville=v_avisiter.get(0); // recherche de la nouvelle ville à visiter
+			long distance=m_distances[villeactuelle][v_avisiter.get(0)];
+			for ( int u : v_avisiter) {//recherche parmis toutes les villes non visitees
+				if (distance>m_distances[villeactuelle][u]) {
+					distance = m_distances[villeactuelle][u];
+					ville=u;
+				}
+			}
+				v_visites.add(ville);
+				Integer city = ville;
+				v_avisiter.remove(city);
+				k+=1;
+				Sol.setCityPosition(ville, k);
+				SolutionListe.add(ville);
+				villeactuelle=ville;
+		}
+
+		Sol.evaluate();
+		this.m_solution=Sol;
+		SolutionListe.add(villeInitiale);
+		return SolutionListe;
 	}
 	
-	
-	while (v_avisiter.size()>0) {
-		int ville=v_avisiter.get(0); // recherche de la nouvelle ville à visiter
-		long distance=m_distances[villeactuelle][v_avisiter.get(0)];
-		for ( int u : v_avisiter) {//recherche parmis toutes les villes non visitees
-			if (distance>m_distances[villeactuelle][u]) {
-				distance = m_distances[villeactuelle][u];
-				ville=u;
+	public ArrayList<Integer> Secondopt(ArrayList<Integer> SolutionListe){
+		int n = SolutionListe.size();
+		
+		for ( int i = 0; i < n-1; i++ ) {
+			for ( int j = i+1; j < n; j++ ) {
+				long nb = calculDistance(SolutionListe);
+				Integer a = SolutionListe.get(i);
+				SolutionListe.set(i, SolutionListe.get(j));
+				SolutionListe.set(j, a);
+				if (nb < calculDistance(SolutionListe)) {
+					Integer b = SolutionListe.get(i);
+					SolutionListe.set(i, SolutionListe.get(j));
+					SolutionListe.set(j, b);
+				}
 			}
 		}
-		v_visites.add(ville);
-		Integer city = ville;
-		v_avisiter.remove(city);
-		k+=1;
-		Sol.setCityPosition(ville, k);
-		villeactuelle=ville;
+		return SolutionListe;
 	}
-
-	Sol.evaluate();
-	this.m_solution=Sol;
+	
+	public long calculDistance (ArrayList<Integer> chemin) {
+		int n = m_instance.getNbCities();
+		long[][] distance = m_instance.getDistances();
+		long distance1=0;//calcul de la distance parcouru par la fourmis
+		for (int j=0;j<n-1;j++){
+			distance1+=distance[chemin.get(j)][chemin.get(j+1)];
+		}
+		return distance1;
+	
+	}
 }
